ddf759f58d28153f7474b42a4652ff88e335a2e6
Phyllis Kan <pyk9q7@gmail.com>
2019-08-05 16:40:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,8 +39,8 @@ def map_to_square(array)
   return new
 end
 
-def reduce_to_total(array, starting_point)
-  array.reduce(0){|sum, num| sum + num}
+def reduce_to_total(array, starting_point=0)
+  array.reduce(starting_point){|sum, num| sum + num}
 end
 
 def reduce_to_all_true(array)
