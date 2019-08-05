404d94e747b0222242e558013e0fd1f5c263442b
Phyllis Kan <pyk9q7@gmail.com>
2019-08-05 16:22:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,19 +1,19 @@
 # My Code here....
-def map_to_negativize(source_array)
+def map_to_negativize(array)
   new = []
   i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 )
+  while i < array.length do
+    new.push(array[i] * -1)
     i += 1
   end
   return new
 end
 
-def map_to_no_change(source_array)
+def map_to_no_change(array)
   new = []
   i = 0
-  while i < source_array.length do
-    new.push( source_array[i] )
+  while i < array.length do
+    new.push(array[i])
     i += 1
   end
   return new
